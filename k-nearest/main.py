5f1642dea8a910be3fae87a4fff21891b7be9c7e
Wilco <57558724+WilcoMatthijssen@users.noreply.github.com>
2020-09-04 17:21:03 +0200

    Create main.py

---
+++ k-nearest/main.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+
+dates = np.genfromtxt("days.csv", delimiter=";", usecols=[1])
+print(dates)
+labels = []
+label: str
+for label in dates:
+  if label < 20000301:
+    labels.append("winter")
+  elif 20000301 <= label < 20000601:
+    labels.append("lente")
+  elif 20000601 <= label < 20000901:
+    labels.append("zomer")
+  elif 20000901 <= label < 20001201:
+    labels.append("herfst")
+  else: # from 01-12 to end of year
+    labels.append("winter")

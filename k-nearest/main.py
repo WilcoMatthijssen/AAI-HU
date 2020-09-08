63617cd6cbc889ed118b5008be2042424cfc6a2e
Wilco <57558724+WilcoMatthijssen@users.noreply.github.com>
2020-09-08 10:22:03 +0200

    Added validation dataset

--- k-nearest/main.py
+++ k-nearest/main.py
@@ -1,11 +1,18 @@
 import numpy as np
+import numpy as np
+#datasetfile = "dataset1.csv"
+datasetfile = "validation1.csv"
 
 
-dates = np.genfromtxt("days.csv", delimiter=";", usecols=[1])
-print(dates)
+data = np.genfromtxt(datasetfile, delimiter=";", usecols=[1,2,3,4,5,6,7], converters={5: lambda s: 0 if s == b"-1" else float(s), 7: lambda s: 0 if s == b"-1" else float(s)})
+
+
+
+
+dates = np.genfromtxt(datasetfile, delimiter=";", usecols=[0])
 labels = []
-label: str
 for label in dates:
+  print(type(label))
   if label < 20000301:
     labels.append("winter")
   elif 20000301 <= label < 20000601:
@@ -16,3 +23,6 @@ for label in dates:
     labels.append("herfst")
   else: # from 01-12 to end of year
     labels.append("winter")
+print( labels)
+
+

e791c156aa8d1948d4e3c22d0ea009faf32c5a17
Wilco <57558724+WilcoMatthijssen@users.noreply.github.com>
2020-09-08 17:44:23 +0200

    Normalisatie geimplementeerd

--- k-nearest/main.py
+++ k-nearest/main.py
@@ -1,19 +1,12 @@
 import numpy as np
-import numpy as np
-#datasetfile = "dataset1.csv"
-datasetfile = "validation1.csv"
 
 
-data = np.genfromtxt(datasetfile, delimiter=";", usecols=[1,2,3,4,5,6,7], converters={5: lambda s: 0 if s == b"-1" else float(s), 7: lambda s: 0 if s == b"-1" else float(s)})
+def load_data(filename):
+    data = np.genfromtxt(filename, delimiter=";", usecols=[1, 2, 3, 4, 5, 6, 7],
+                         converters={5: lambda s: 0 if s == b"-1" else float(s),
+                                     7: lambda s: 0 if s == b"-1" else float(s)})
 
-
-datasetfile = "dataset1.csv"
-validationfile = "validation1.csv"
-
-<<<<<<< HEAD
-def loadData():
-    data = np.genfromtxt(datasetfile, delimiter=";", usecols=[1,2,3,4,5,6,7], converters={5: lambda s: 0 if s == b"-1" else float(s), 7: lambda s: 0 if s == b"-1" else float(s)})
-    dates = np.genfromtxt(datasetfile, delimiter=";", usecols=[0])
+    dates = np.genfromtxt(filename, delimiter=";", usecols=[0])
     labels = []
     for label in dates:
         if label < 20000301:
@@ -24,58 +17,47 @@ def loadData():
             labels.append("zomer")
         elif 20000901 <= label < 20001201:
             labels.append("herfst")
-        else: # from 01-12 to end of year
+        else:  # from 01-12 to end of year
             labels.append("winter")
-    return data
+    return data, labels
 
-def normalisatie(input_value = None):
-    data = loadData()
 
+def normalisation():
+    data_set, data_labels = load_data("dataset1.csv")
+    validation_set, validation_labels = load_data("validation1.csv")
+
+    norm_range = get_normalisation(data_set)
+
+    norm_data = normalize(data_set, norm_range)
+    norm_validation_data = normalize(validation_set, norm_range)
+
+    np.set_printoptions(threshold=np.inf)
+    print(np.round(norm_data, 2))
+
+
+def get_distance(a, b):
+    # geen idee of dit zo hoort of nodig is. (wordt niet gebruikt)
+    total = 0
+    for i in range(7):
+        total += (a[i]-b[i])**2
+    return np.sqrt(total)
+
+
+def get_normalisation(data):
     max_values = []
     min_values = []
-    
-    for i in range(len(data[0])):
-        max = 0
-        min = float("inf")
-        for j in data:
-            value = j[i]
-            max = value if value > max else max
-            min = value if value < min else min
-        max_values.append(max)
-        min_values.append(min)
-    for line in applyNormalisatie(min_values,max_values):
-        for value in line:
-            print(value, end='\t')
-        print()
-		
+    for column in data.T:
+        max_values.append(np.max(column))
+        min_values.append(np.min(column))
+    return max_values, min_values
 
-def applyNormalisatie(min_values, max_values, data = loadData()):
-    for i in range(len(data[0])):
-        max = max_values[i]
-        min = min_values[i]
-        for j in data:
-            j[i] = (j[i] - min) / (max - min)
-    #print(repr(data))
+
+def normalize(data, norm_range):
+    min_values, max_values = norm_range
+    for i in range(len(min_values)):
+        for feature in data:
+            feature[i] = (feature[i] - min_values[i]) / (max_values[i] - min_values[i])
     return data
-            
-normalisatie()
-=======
-
-dates = np.genfromtxt(datasetfile, delimiter=";", usecols=[0])
-labels = []
-for label in dates:
-  print(type(label))
-  if label < 20000301:
-    labels.append("winter")
-  elif 20000301 <= label < 20000601:
-    labels.append("lente")
-  elif 20000601 <= label < 20000901:
-    labels.append("zomer")
-  elif 20000901 <= label < 20001201:
-    labels.append("herfst")
-  else: # from 01-12 to end of year
-    labels.append("winter")
-print( labels)
 
 
->>>>>>> 63617cd6cbc889ed118b5008be2042424cfc6a2e
+normalisation()

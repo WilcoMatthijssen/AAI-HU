653a6acf7ec8fa45d88f2c8d2127811186571104
Twan Hoogveld <twan.hoogveld@student.hu.nl>
2020-09-09 17:30:38 +0200

    updated, more speed, added better year

--- k-nearest/main.py
+++ k-nearest/main.py
@@ -1,6 +1,6 @@
 import numpy as np
 from collections import Counter
-
+import time
 
 def load_data(filename):
     data = np.genfromtxt(
@@ -15,28 +15,21 @@ def load_data(filename):
     dates = np.genfromtxt(filename, delimiter=";", usecols=[0])
     labels = []
     for label in dates:
-        if label < 20000301:
+        label = str(label)[4:]
+        if label < '0301':
             labels.append("winter")
-        elif 20000301 <= label < 20000601:
+        elif '0301' <= label < '0601':
             labels.append("lente")
-        elif 20000601 <= label < 20000901:
+        elif '0601' <= label < '0901':
             labels.append("zomer")
-        elif 20000901 <= label < 20001201:
+        elif '0901' <= label < '1201':
             labels.append("herfst")
         else:  # from 01-12 to end of year
             labels.append("winter")
     return data, labels
 
-
-
-
 def get_distance(a, b):
-    # geen idee of dit zo hoort of nodig is. (wordt niet gebruikt)
-    total = 0
-    for i in range(7):
-        total += (a[i] - b[i])**2
-    return np.sqrt(total)
-
+    return sum(a) - sum(b)
 
 def get_normalisation(data):
     max_values = []
@@ -46,7 +39,6 @@ def get_normalisation(data):
         min_values.append(np.min(column))
     return max_values, min_values
 
-
 def normalize(data, norm_range):
     min_values, max_values = norm_range
     for i in range(len(min_values)):
@@ -54,7 +46,6 @@ def normalize(data, norm_range):
             feature[i] = (feature[i] - min_values[i]) / (
                 max_values[i] - min_values[i])
     return data
-
 
 def k_nearest_neighbour(k, data_point, training_set, training_labels):
     distances = []
@@ -65,12 +56,10 @@ def k_nearest_neighbour(k, data_point, training_set, training_labels):
     distances.sort()
 
     # Get K lowest distances classifications
-    classifications =[ i[1] for i in distances[:k] ]
+    classifications = [ i[1] for i in distances[:k] ]
 
     # Return most common classification
     return Counter(classifications).most_common(1)[0][0]
-
-
 
 def find_best_k(validation_set, validation_labels, data_set, data_labels):
     # voor elke 1 tot len val_set (K)
@@ -79,46 +68,40 @@ def find_best_k(validation_set, validation_labels, data_set, data_labels):
     #     kijk of hoogste match is bereikt
     #     gebruik daar de beste van
 
-
-
     best_k = 0
-    highest_value = 0.0
-
-    accurateness = {}
-    for k in range(1, len(validation_set)):
-        print("yeet")
+    best_value = 0
+    for k in range(1, len(validation_set), 10 ):
+        classifications = []
+        for validation_point in validation_set:
+            classifications.append(k_nearest_neighbour(k, validation_point,
+                                                       data_set, data_labels))
         matches = 0
-        for validation_point, validation_label in zip(validation_set, validation_labels):
-            if validation_label == k_nearest_neighbour(k, validation_point, data_set, data_labels):
+        for classification_index, _ in enumerate(classifications):
+            if classifications[classification_index] == validation_labels[classification_index]:
                 matches += 1
-        accurateness[k] = matches / len(validation_set) * 100
-
-    return accurateness
-
-
-
-
+        result = (matches * 100) / len(validation_labels)
+        print(k,result)
+        if result > best_value:
+            best_value = result
+            best_k = k
+    print(best_k,best_value)
+    return best_k
 
 if __name__ == '__main__':
-    print("start")
-    data_set, data_labels = load_data("dataset1.csv")
-    validation_set, validation_labels = load_data("validation1.csv")
+    #Had to use r'' because \v is a character.
+    data_set, data_labels = load_data(r'k-nearest\dataset1.csv')
+    validation_set, validation_labels = load_data(r'k-nearest\validation1.csv')
 
     normalize_range = get_normalisation(data_set)
 
     normalized_data = normalize(data_set, normalize_range)
     normalized_val_data = normalize(validation_set, normalize_range)
 
-    #best_k = find_best_k(normalized_data, data_labels, normalized_val_data, validation_labels)
-   # print(k_nearest_neighbour(3,normalized_val_data[0], normalized_data, data_labels))
-    print(find_best_k(normalized_val_data, validation_labels, normalized_data, data_labels))
-
+    best_k = find_best_k(normalized_data, data_labels, normalized_val_data, validation_labels)
+    print(k_nearest_neighbour(3,normalized_val_data[0], normalized_data, data_labels))
 
     np.set_printoptions(threshold=np.inf)
     #print(np.round(normalized_data, 2))
-
-   
-    print("done")
 
     #print(data_labels)
 

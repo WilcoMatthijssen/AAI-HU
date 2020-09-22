306869d8a5ca40e4b2e1bada1d84007e33cfea0d
Twan Hoogveld <twan.hoogveld@student.hu.nl>
2020-09-22 14:44:06 +0200

    update

--- nn/main.py
+++ nn/main.py
@@ -6,31 +6,29 @@ import math
 def assignment4_1():
     """
     NOR GATE
-    3 INPUTS 0.1
-    1 OUTPUT 0/1
-    THRESHOLD 0.1
+    3 INPUTS
+    1 OUTPUT 
+    THRESHOLD 
     """
     sig = lambda x: 1/(1+math.exp(-x))
-    w = np.array([15,-10,-10])
-    def NOT(x,w,act):
-        return (act(w.dot(x)))
-    x = np.array([1,0,1])
-    return NOT(x,w,sig)
+    w = np.array([1,1,1])
 
-def gib_data():
-    def data_to_csv_file(filename):
-        return genfromtxt(filename,dtype=float,delimiter=',',usecols=(0,1,2,3))
-        
-    def normalize(data):
-        max_values = [np.max(column) for column in data.T]
-        min_values = [np.min(column) for column in data.T]
-        for feature in data:
-            for i in range(len(feature)):
-                feature[i] = (feature[i] - min_values[i]) / (max_values[i] - min_values[i])
+    def OR(x,w,act):
+        return act(w.dot(x))
+
+    x = np.array([1,0,0])
+    return not bool(round(OR(x,w,sig)))
+
+def data_to_csv_file(filename):
+    return genfromtxt(filename,dtype=float,delimiter=',',usecols=(0,1,2,3))
     
-    data = data_to_csv_file("iris.data")
-    normalize(data)
-    return data
+def normalize(data):
+    max_values = [np.max(column) for column in data.T]
+    min_values = [np.min(column) for column in data.T]
+    for feature in data:
+        for i in range(len(feature)):
+            feature[i] = (feature[i] - min_values[i]) / (max_values[i] - min_values[i])
+
     
 class Neuron:
     def ___init____(self):
@@ -45,7 +43,8 @@ class Adder:
         self.name = "Adder"
 
 if __name__ == "__main__":
-    a = gib_data()
-    print(a)
+    data = data_to_csv_file("iris.data")
+    normalize(data)
+    print(assignment4_1())
 
 

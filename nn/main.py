0e78d1ef406956787fe83b598a7e5a913f1afe7e
Wilco <wilco@matthijssen.xyz>
2020-10-02 20:00:27 +0200

    added NN implementation

--- nn/main.py
+++ nn/main.py
@@ -1,50 +1,129 @@
+import time
 import numpy as np
-from numpy import genfromtxt
-import csv
-import math
+import random
 
-def assignment4_1():
-    """
-    NOR GATE
-    3 INPUTS
-    1 OUTPUT 
-    THRESHOLD 
-    """
-    sig = lambda x: 1/(1+math.exp(-x))
-    w = np.array([1,1,1])
 
-    def OR(x,w,act):
-        return act(w.dot(x))
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
 
-    x = np.array([1,0,0])
-    return not bool(round(OR(x,w,sig)))
 
-def data_to_csv_file(filename):
-    return genfromtxt(filename,dtype=float,delimiter=',',usecols=(0,1,2,3))
-    
-def normalize(data):
-    max_values = [np.max(column) for column in data.T]
-    min_values = [np.min(column) for column in data.T]
-    for feature in data:
-        for i in range(len(feature)):
-            feature[i] = (feature[i] - min_values[i]) / (max_values[i] - min_values[i])
+def sigmoid_der(x):
+    return sigmoid(x) * (1 - sigmoid(x))
 
-    
+
+class NeuralNetwork:
+
+    def __init__(self, input_layer_size, output_layers_size, isSigmoid=True, learningRate=0.1):
+        self.learnRate = learningRate
+
+        self.input_layers = [Neuron() for _ in range(input_layer_size)]
+
+        self.hidden_layers = []
+        for i in range(input_layer_size):
+            weights = [random.uniform(-1, 1) for _ in range(input_layer_size)]
+            self.hidden_layers.append(
+                Neuron(isSigmoid, weights, random.uniform(-1, 1), random.uniform(-1, 1), self.input_layers))
+
+        self.output_layers = []
+        for i in range(output_layers_size):
+            weights = [random.uniform(-1, 1) for _ in range(input_layer_size)]
+            self.output_layers.append(
+                Neuron(isSigmoid, weights, random.uniform(-1, 1), random.uniform(-1, 1), self.hidden_layers))
+
+    def predict(self, input_data):
+        for input_l, input_d in zip(self.input_layers, input_data):
+            input_l.output = input_d
+
+        for hidden in self.hidden_layers:
+            hidden.feedForward()
+
+        return [out.feedForward() for out in self.output_layers]
+
+    def backpropagation(self, desired_output):
+        for i in range(len(self.output_layers)):
+            self.output_layers[i].error = (desired_output[i] - self.output_layers[i].output) * sigmoid_der(self.output_layers[i].neuronInput)
+
+        for i in range(len(self.hidden_layers)):
+            error_sum = sum(output.error * output.weights[i] for output in self.output_layers)
+            self.hidden_layers[i].error = sigmoid_der(self.hidden_layers[i].neuronInput) * error_sum
+
+    def update(self):
+        for o_layer in self.output_layers:
+            o_layer.update(self.learnRate)
+
+        for h_layer in self.hidden_layers:
+            h_layer.update(self.learnRate)
+
+    def train(self, input_data, desired_output, epochs):
+        for _ in range(epochs):
+            for data_in, data_out in zip(input_data, desired_output):
+                self.predict(data_in)
+                self.backpropagation(data_out)
+                self.update()
+
+
 class Neuron:
-    def ___init____(self):
-        self.name = "Neuron"
-         
-class Perceptron:
-    def ___init____(self):
-        pass
-    
-class Adder:
-    def ___init____(self):
-        self.name = "Adder"
+
+    def __init__(self, isSigmoid=True, weights=None, threshold=None, bias=None, previous_layer=None):
+        self.previous_layer = previous_layer
+        self.weights = weights
+
+        self.error = 0
+        self.bias = bias
+        self.threshold = threshold
+        self.output = 0
+        self.neuronInput = 0
+        self.isSigmoid = isSigmoid
+
+    def feedForward(self):
+        self.neuronInput = sum(val.output * w for val, w in zip(self.previous_layer, self.weights))
+        if self.isSigmoid:
+            self.neuronInput += self.bias
+            self.output = sigmoid(self.neuronInput)
+        else:
+            self.output = 1 if self.neuronInput >= self.threshold else 0
+        return self.output
+
+    def update(self, learn_rate):
+        for i in range(len(self.weights)):
+            self.weights[i] += learn_rate * self.error * self.previous_layer[i].output
+
+        self.bias += learn_rate * self.error
+
 
 if __name__ == "__main__":
-    data = data_to_csv_file("iris.data")
-    normalize(data)
-    print(assignment4_1())
+    random.seed(0)
+    trainingData = np.genfromtxt("iris.data", delimiter=",", usecols=[0, 1, 2, 3])
+    trainingDataOutput = np.genfromtxt("iris.data", delimiter=",", usecols=[4], dtype=str)
+
+    convertedOutput = []
+
+    for i in range(len(trainingDataOutput)):
+        if trainingDataOutput[i] == "Iris-setosa":
+            convertedOutput.append([1, 0, 0])
+        if trainingDataOutput[i] == "Iris-versicolor":
+            convertedOutput.append([0, 1, 0])
+        if trainingDataOutput[i] == "Iris-virginica":
+            convertedOutput.append([0, 0, 1])
+
+    irisNetwork = NeuralNetwork(4, 3, True, 0.05)
+    irisNetwork.train(trainingData, convertedOutput, 1000)
+    runtime = time.perf_counter()
+    print(f"Training took {runtime:0.4f} seconds")
+
+    correctCounter = 0
+    for i in range(len(trainingData)):
+        tempOutput = irisNetwork.predict(trainingData[i])
+        correctCounter += convertedOutput[i][tempOutput.index(max(tempOutput))]
 
 
+    print("NN was: ", (correctCounter / len(trainingData) * 100), "% correct")
+
+    dataSetIndex = 40
+
+    print(f"\nNN predicts index {dataSetIndex} is {trainingDataOutput[dataSetIndex]}")
+
+    tempNetworkOutput = irisNetwork.predict(trainingData[dataSetIndex])
+    print(round(tempNetworkOutput[0], 2) * 100,"% Iris-Setosa")
+    print(round(tempNetworkOutput[1], 2) * 100,"% Iris-Versicolor")
+    print(round(tempNetworkOutput[2], 2) * 100,"% Iris-Virginica\n")

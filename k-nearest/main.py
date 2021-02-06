ac9d6a85a6524e20bde0970319059dc6b08bbb1a
Wilco <wilco@matthijssen.xyz>
2021-02-07 00:04:09 +0100

    Cooler file loading and normalisation

--- k-nearest/main.py
+++ k-nearest/main.py
@@ -3,7 +3,7 @@ from collections import Counter
 import time
 from functools import lru_cache
 
-60, 120
+
 
 def load_data(filename):
     """ Loads a csv file for use by k_nn
@@ -21,33 +21,27 @@ def load_data(filename):
             7: lambda s: 0 if s == b"-1" else float(s)
         })
 
-    dates = np.genfromtxt(filename, delimiter=";", usecols=[0])
-    labels = []
-    for label in dates:
-        label = str(label)[4:]
-        if '0301' <= label < '0601':
-            labels.append("lente")
-        elif '0601' <= label < '0901':
-            labels.append("zomer")
-        elif '0901' <= label < '1201':
-            labels.append("herfst")
-        else:  # from 01-12 to end of year
-            labels.append("winter")
-    return data, tuple(labels)
+    dates_with_years =  np.genfromtxt(filename, delimiter=";", usecols=[0])
+    dates = np.array([str(label)[4:] for label in dates_with_years])
+
+    dates[('0301' <= dates) & (dates < '0601')] = "lente"
+    dates[('0601' <= dates) & (dates < '0901')] = "zomer"
+    dates[('0901' <= dates) & (dates < '1201')] = "herfst"
+    dates[np.in1d(dates, ["lente", "zomer", "herfst"], invert=True)] = "winter"
+
+    return tuple(data), tuple(dates)
 
 
-def get_normalisation(data_set):
-    """ Finds the normalisation of data_set and returns it as a List of highest values for each attribute
-        and a list of the lowest value for each attribute.
-    """
-    max_values = np.array([np.max(column) for column in data_set.T])
-    min_values = np.array([np.min(column) for column in data_set.T])
-    return max_values, min_values
+def get_normalized(data_set, *other_data_sets):
+    """ Gets normalisation for data_set and applies it to it and other_data_sets """
+    max_values = np.amax(data_set)
+    min_values = np.amin(data_set)
+    range_values = max_values - min_values
 
+    yield tuple(tuple((feature - min_values) / range_values) for feature in data_set)
 
-def normalize(min_values, max_values, data_set):
-    """ Applies norm_range to the data_set to normalize it."""
-    return tuple(tuple((feature - min_values) / (max_values - min_values)) for feature in data_set)
+    for other_set in other_data_sets:
+        yield tuple(tuple((feature - min_values) / range_values) for feature in other_set)
 
 
 def get_distance(point_a, point_b):
@@ -87,13 +81,12 @@ def find_best_k(data_set, data_labels, validation_set, validation_labels):
     Returns:
         int: k with best accuracy
     """
-    best_k = 0
+    best_k     = 0
     best_value = 0
 
     for k in range(1, len(validation_set)):
-
         classifications = [k_nearest_neighbour(k, point, data_set, data_labels) for point in validation_set]
-        result = np.mean(np.array(classifications) == np.array(validation_labels))
+        result          = np.mean(np.array(classifications) == np.array(validation_labels))
 
         print(f"k: {k},\taccuracy: {round(result * 100) }%")
 
@@ -111,11 +104,8 @@ if __name__ == '__main__':
     validation_set, validation_labels = load_data(r'validation1.csv')
     days, _ = load_data(r'days.csv')
 
-    # get normalisation  and apply to dataset
-    min_values, max_values = get_normalisation(validation_set)
-    data_set       = normalize(min_values, max_values, data_set)
-    validation_set = normalize(min_values, max_values, validation_set)
-    days           = normalize(min_values, max_values, days)
+    # get normalisation and apply to dataset
+    data_set, validation_set, days = get_normalized(data_set, validation_set, days)
 
     # find best k
     best_k = find_best_k(data_set, data_labels, validation_set, validation_labels)
